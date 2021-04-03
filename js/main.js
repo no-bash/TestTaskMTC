7bbd35437dfb762e1e93af531d5c947e7c3e5827
no-bash <blunskay@gmail.com>
2021-04-03 20:10:43 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -12,8 +12,27 @@ const swiper = new Swiper('.swiper-container', {
       spaceBetween: 20
     },
     640: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    1023: {
       slidesPerView: 3,
       spaceBetween: 40
     }
   }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  let hamburger = document.getElementById('openMenu');
+  hamburger.addEventListener('click', function() {
+    let menu = document.getElementById('adaptiveMenu');
+    menu.style.width = "100vw";
+  })
+
+  let closeIcon = document.getElementById('closeIcon');
+
+  closeIcon.addEventListener('click', function() {
+    let menu = document.getElementById('adaptiveMenu');
+    menu.style.width = "0";
+  });
+})
